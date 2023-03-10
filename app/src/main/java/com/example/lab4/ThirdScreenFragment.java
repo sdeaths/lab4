ea42accd384011edf81e141868f73b3135f64fe8
Elizaveta Urvanova <urvanova03@mail.ru>
2023-03-10 15:58:00 +0300

    feat: maked RecyclerView with Image and Text

--- app/src/main/java/com/example/lab4/ThirdScreenFragment.java
+++ app/src/main/java/com/example/lab4/ThirdScreenFragment.java
@@ -11,6 +11,9 @@ import androidx.fragment.app.Fragment;
 
 import com.example.lab4.databinding.FragmentThirdScreenBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThirdScreenFragment extends Fragment {
     private FragmentThirdScreenBinding binding;
 
@@ -19,5 +22,17 @@ public class ThirdScreenFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentThirdScreenBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        List<CosmeticRecyclerItem> listItems = new ArrayList<>();
+        String[] categoryTitles = {"Косметика", "Еда", "Акссесуары"};
+        for (int i = 0; i < 200; i++) {
+            listItems.add(new CosmeticRecyclerItem(R.drawable.lipstick, categoryTitles[(int) (Math.random() * categoryTitles.length)]));
+        }
+        CategoryRecyclerViewAdapter adapter = new CategoryRecyclerViewAdapter(getContext(), listItems);
+        binding.recyclerView.setAdapter(adapter);
     }
 }
